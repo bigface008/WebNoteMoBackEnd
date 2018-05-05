a0c53d9ecbfa23810faf222027062208fb9171e2
bigface008 <bigface00@sjtu.edu.cn>
2018-05-05 14:17:55 +0800

    Syc

---
+++ src/Servlet/Login.java
@@ -0,0 +1,63 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Entity.UserEntity;
+
+import Util.HibernateUntil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+@WebServlet("/checkuser")
+public class Login extends HttpServlet{
+
+    public Login() { super(); }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        try {
+            PrintWriter out = response.getWriter();
+
+            String username = request.getParameter("name");
+            String password = request.getParameter("pwd");
+            Boolean isValid = login(username, password);
+
+            out.println(isValid);
+            out.flush();
+            out.close();
+        } catch (Exception ex) {
+            if (ServletException.class.isInstance(ex)) {
+                throw (ServletException) ex;
+            } else {
+                throw new ServletException(ex);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Boolean login(String username, String password) {
+        Boolean isValid = false;
+        Session session = HibernateUntil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        List<UserEntity> result = (List<UserEntity>) session
+                .createQuery("select userName from UserEntity where userName = :name and userPassword = :pwd")
+                .setParameter("name", username).setParameter("pwd", password).list();
+        tx.commit();
+        if (result.size() > 0)
+            isValid = true;
+        else
+            isValid = false;
+        session.close();
+        return isValid;
+    }
+}
---
+++ src/Util/HibernateUntil.java
@@ -0,0 +1,13 @@
+package Util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUntil {
+
+    private static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
